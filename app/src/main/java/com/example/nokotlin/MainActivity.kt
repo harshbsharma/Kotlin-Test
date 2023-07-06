188d374e898b523bd89f48075e8c4bda6ae6f21b
Harsh Sharma <harshbsharma1209@gmail.com>
2023-07-06 15:12:33 +0530

    Inital Commit of the repo

---
+++ app/src/main/java/com/example/nokotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.nokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+
+    public fun click(View : View)
+    {
+        setContentView(R.layout.secondlayout)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
