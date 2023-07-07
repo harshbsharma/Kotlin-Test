65dfb7aa57809605a7e831c726e4fc087848f605
Harsh Sharma <harshbsharma1209@gmail.com>
2023-07-07 16:46:43 +0530

    Made Login Page XML

--- app/src/main/java/com/example/nokotlin/SecondActivity.kt
+++ app/src/main/java/com/example/nokotlin/SecondActivity.kt
@@ -5,20 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.widget.ButtonBarLayout
+import org.w3c.dom.Text
 
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
-    public fun click(View : View)
-    {
-        val ordermsg: String  ="com.example.nokotlin.msg"
-        intent= Intent (this,SecondActivity::class.java)
-        intent.putExtra(ordermsg, "Harsh")
-        startActivity(intent)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val data : Bundle? = intent.extras
+        val string : String? = intent.getStringExtra("msg")
+//        val view : TextView = findViewById()
+
+
     }
 }
