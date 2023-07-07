f38ecf0e8f07f8d0fff1bb1cdb951ee0a7c337c1
Harsh Sharma <harshbsharma1209@gmail.com>
2023-07-07 19:26:37 +0530

    Added Splash Screen

--- app/src/main/java/com/example/nokotlin/MainActivity.kt
+++ app/src/main/java/com/example/nokotlin/MainActivity.kt
@@ -3,22 +3,37 @@ package com.example.nokotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
+import android.view.WindowManager
+import android.view.WindowManager.*
 import android.widget.Button
+import android.widget.Toolbar
 import androidx.appcompat.widget.ButtonBarLayout
 
 
 class MainActivity : AppCompatActivity() {
 
-    public fun click(View : View)
-    {
-        val ordermsg: String  ="com.example.nokotlin.msg"
-        intent= Intent (this,SecondActivity::class.java)
-        intent.putExtra(ordermsg, "Harsh")
-        startActivity(intent)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       setContentView(R.layout.activity_main)
+//        val toolbar : androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        window.addFlags(LayoutParams.FLAG_FULLSCREEN)
+        val btn:Button = findViewById(R.id.signinbtn)
+        btn.setOnClickListener {
+
+            val intent = Intent (this,splash_screen::class.java)
+            startActivity(intent)
+
+
+        }
+//        setSupportActionBar(toolbar)
     }
+//
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.toolbar_menu, menu)
+//        return true
+//
+//    }
+
 }
